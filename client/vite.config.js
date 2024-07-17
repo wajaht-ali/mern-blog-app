f02cbcb49add4e707e8a6aaf147b4cbb3febf13c
wajaht-ali <contact.chwajahat@gmail.com>
2024-07-17 16:07:34 +0500

    Vite cofiguration debugging

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://mern-blog-app-api-rho.vercel.app",
+      '/api': {
+        target: 'https://mern-blog-app-api-rho.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 });
