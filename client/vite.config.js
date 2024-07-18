dd0476d5b76666784330814f3f673132ce5dfb8e
wajaht-ali <contact.chwajahat@gmail.com>
2024-07-18 12:05:08 +0500

    deployment dubugged successfully

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://mern-blog-app-api.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
+})
